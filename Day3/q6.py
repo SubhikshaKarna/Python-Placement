71bd233bffe25c087b513757b47923fae8105a8d
Subhiksha Karna <149041362+SubhikshaKarna@users.noreply.github.com>
2024-06-10 16:29:11 +0530

    Day3 Commit

--- Day3/q6.py
+++ Day3/q6.py
@@ -6,11 +6,3 @@ lst_u= list(set(list1) | set(list2))
 print(lst_i)
 print(lst_u)
 
-# Second Method
-list1=[1,2,6,8]
-list2=[1,2,4,9,7]
-m=[]
-i=j=0
-while(i<len(list1) and j<len(l2)):
-    if list1[i]<list2[j]:
-        m.append(list1[i])
--- Day3/q7.py
+++ Day3/q7.py
@@ -1,6 +1,23 @@
 # 7)To Merge 2 sorted list into a single sorted list
-list1=[1,3,6,8]
-list2=[4,9,7]
-list1.extend(list2)
-list1.sort()
-print(list1)
+lst1=[1,3,6,8]
+lst2=[4,9,7]
+lst1.extend(lst2)
+lst1.sort()
+print(lst1)
+
+
+# Second Method
+list1=[1,2,6,8]
+list2=[3,4,9,7]
+m=[]
+i=j=0
+while(i<len(list1) and j<len(list2)):
+    if list1[i]<list2[j]:
+        m.append(list1[i])
+        i=i+1
+    else:
+        m.append(list2[j])
+        j=j+1
+m.extend(list2[j:])
+m.extend(list2[i:])
+print(m)
